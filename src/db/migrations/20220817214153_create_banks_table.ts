1301954029e6de53a003712cfac36ae73138bb80
Kehinde Hussein, Fasunle <kfasunle@gmail.com>
2022-08-18 08:49:47 +0100

    fix bank migration file

--- src/db/migrations/20220817214153_create_banks_table.ts
+++ src/db/migrations/20220817214153_create_banks_table.ts
@@ -3,14 +3,14 @@ import { Knex } from 'knex';
 export async function up( knex: Knex ): Promise<void> {
     return knex.schema.createTable( 'banks', ( table ) => {
         table.uuid( 'id' ).primary();
-        table.string( 'name', 256 ).notNullable().unique();
-        table.string( 'description', 256 );
-        table.string( 'owner', 256 ).notNullable();
-        table.string( 'manager', 256 ).notNullable();
+        table.string( 'name', 30 ).notNullable().unique();
+        table.string( 'description', 100 );
+        table.string( 'owner', 30 ).notNullable();
+        table.string( 'manager', 30 ).notNullable();
         table.integer( 'numberOfWorkers' ).defaultTo( 1 );
-        table.string( 'country', 256 ).defaultTo( 'Nigeria' );
-        table.string( 'address', 256 ).notNullable();
-        table.string( 'password', 256 ).notNullable();
+        table.string( 'country', 30 ).defaultTo( 'Nigeria' );
+        table.string( 'address', 50 ).notNullable();
+        table.string( 'password', 50 ).notNullable();
     } );
 }
 
