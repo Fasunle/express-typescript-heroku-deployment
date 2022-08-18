bdae2780f8161843419417cf3f044f36f7534b4b
Kehinde Hussein, Fasunle <kfasunle@gmail.com>
2022-08-18 09:12:49 +0100

    add get bank route

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,6 @@
 import express from 'express';
 import { config } from 'dotenv';
-
+import knex from './db/connectdb';
 
 const app = express();
 config();
@@ -9,6 +9,13 @@ const PORT = process.env.PORT || 5000;
 
 app.get( '/', ( req, res ) => {
     return res.status( 200 ).send( "Hello World from NodeJS APP" );
+} );
+
+app.get( '/bank', async ( req, res ) => {
+
+    const banks = await knex.select( '*' ).from( 'banks' );
+
+    res.json( banks );
 } );
 
 ( () => {
